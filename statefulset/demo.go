e9d42af07aa38944270687e6c10b425e33e9af65
Liu Gang <SENSETIME\liugang1@shst18045066.domain.sensetime.com>
2019-12-23 11:22:18 +0800

    Add config parse.

--- statefulset/demo.go
+++ statefulset/demo.go
@@ -1,18 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+type Config struct {
+	MasterMode bool `json:"masterMode"`
+}
+
+var masterMode bool
+
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "Blog:%s \nWechat:%s", "www.flysnow.org", "flysnow_org")
+	var mode string
+	if masterMode {
+		mode = "master"
+	} else {
+		mode = "slave"
+	}
+	fmt.Fprintf(w, "hello, %s!\n", mode)
 }
 
 func main() {
+	data, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		fmt.Println("read config file error : ", err)
+		os.Exit(-1)
+	}
+	mode := new(Config)
+	err = json.Unmarshal(data, mode)
+	if err != nil {
+		fmt.Println("parse json file error : ", err)
+		os.Exit(-1)
+	}
+	masterMode = mode.MasterMode
+	fmt.Println(masterMode)
 	router := httprouter.New()
 	router.GET("/", Index)
 	log.Fatal(http.ListenAndServe(":8080", router))
